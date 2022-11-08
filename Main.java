1527f1b85a6e5e29f002d219bc7970e76ec14a1d
ryoutarou <r.suzuki.sys22@morijyobi.ac.jp>
2022-11-08 10:38:33 +0900

    add .gitignore

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+        public static void main(String[] args) {
+                Scanner sc1 = new Scanner(System.in);
+                System.out.println("身長:");
+                double a = sc1.nextDouble();
+                Scanner sc2 = new Scanner(System.in);
+                System.out.println("体重:");
+                double b = sc2.nextDouble();
+                double bmi=(b/(a*a));
+                System.out.println(String.format("%.2f",bmi));
+        }
+}
